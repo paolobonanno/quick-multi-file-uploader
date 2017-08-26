751599b8daa555945b60dc401f1bb70e3c1f82db
paolobonanno <31191955+paolobonanno@users.noreply.github.com>
2017-08-26 18:58:53 +0200

    Update quickMultiFileUploader.js
    Refactoring.
    Data are private.
    Upload method is now a function.
    Add resetAfterUpload option and ajax callbacks.

--- quickMultiFileUploader.js
+++ quickMultiFileUploader.js
@@ -1,107 +1,186 @@
 (function ($) {
 
-    if (!("FormData" in window)) {
-        console.error("quickMultiFileUploader: FormData is not supported in you browser");
+    var internalPrefix = "_quickMultiFileUploader";
+
+    var ajaxOptionKeys = Object.keys($.ajaxSetup());
+
+    var ajaxOptions = {
+        method: "post",
+        cache: false,
+        processData: false,
+        contentType: false,
+        complete: function () { },
+        error: function () { },
+        success: function () { },
+        beforeSend: function () { }
+    };
+
+    function showLog(condition, message, logFn) {
+        if (condition) {
+            logFn("quickMultiFileUploader: " + message);
+        }
+
+        return condition;
     }
-    else {
-        var internalPrefix = '_quickMultiFileUploader';
+    
+    function showError(condition, message) {
+        return showLog(condition, message, console.error);
+    }
+
+    function showWarn(condition, message) {
+        return showLog(condition, message, console.warn);
+    }
+
+    function checkOptionsType(options) {
+        if (options === undefined) {
+            options = {};
+        }
+
+        if (options && typeof options === "object") {
+            options = $.extend({}, options);
+        }
+
+        return options;
+    }
+
+    function checkOptionsData(data) {
+        for (var key in data) {
+            if (data[key] === "") {
+                data[key] = true;
+            } else if (data[key] === "false") {
+                data[key] = false;
+            } else if (data[key] === "true") {
+                data[key] = true;
+            }
+        }
+    }
+
+    function quickMultiFileUploader(elem, options) {
+
+        var data = $.extend({}, $.quickMultiFileUploader.defaults);
+        var ajaxData = {};
+
+        function addOptions(options) {
+            for (var dataAttributeKey in options) {
+                var objectToFill = $.inArray(dataAttributeKey, ajaxOptionKeys) !== -1 ? ajaxData : data;
+                objectToFill[dataAttributeKey] = options[dataAttributeKey];
+            }
+        }
+
+        function setOptions(options) {
+            options = checkOptionsType(options);
+            
+            addOptions(elem.dataset);
+            addOptions(options);//eventually overwrite dataset
+
+            $.extend(ajaxData, ajaxOptions);//eventually overwrite with ajaxOptions
+            
+            checkOptionsData(data);
+            checkOptionsData(ajaxData);
+
+            $(elem).off("change", startUpload);
+
+            if (data.uploadOnChange) {
+                $(elem).on("change", startUpload);
+            }
+        }
+
+        function startUpload() {
+
+            if (!showWarn(elem.files.length <= 0, "there are no files loaded")) {
+
+                if (!showError(!($.inArray("url", ajaxData) === -1), "the url must be defined")) {
+
+                    if (!showError(data.useInputName && $(elem).attr("name") === undefined, "the name attribute must be defined")) {
+                        
+                        var elemName = $(elem).attr("name");
+                        var formData = new FormData();
+                        
+                        for (var i = 0; i < elem.files.length; i++) {
+                            if (data.useInputName) {
+                                formData.append(elemName, elem.files[i], elem.files[i].name);
+                            } else {
+                                formData.append(elem.files[i].name, elem.files[i], elem.files[i].name);
+                            }
+                        }
+
+                        //exclude callback
+                        for (var dataKey in data) {
+                            if (typeof data[dataKey] !== "function") {
+                                formData.append(dataKey, data[dataKey]);
+                            }
+                        }
+
+                        if (data.beforeSend(elem, $.map(elem.files, function (el) { return el.name; }), elem.files)) {
+                            var beforeSendCallback = { beforeSend: function (jqXHR, settings) { data.beforeSend(elem, jqXHR, settings); } };
+
+                            $.ajax($.extend({ data: formData }, ajaxData, beforeSendCallback))
+                                .done(function (dataAjax, textStatus, jqXHR) {
+                                    data.onSuccess(elem, dataAjax, textStatus, jqXHR);
+                                })
+                                .fail(function (jqXHR, textStatus, errorThrown) {
+                                    data.onError(elem, jqXHR, textStatus, errorThrown);
+                                })
+                                .always(function (dataAjax, textStatus, jqXHR) {
+                                    if (data.resetAfterUpload) {
+                                        elem.value = "";
+                                    }
+                                    data.onComplete(elem, dataAjax, textStatus, jqXHR);
+                                });
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!showError(!$(elem).is("input") || $(elem).attr("type") !== "file", "this plugin must be used for file input elements")) {
+
+            setOptions(options);
+
+            $(elem).data(internalPrefix, this);
+
+            this.setOptions = setOptions;
+            this.upload = startUpload;
+            this.d = function () { return data; };
+        }
+    }
+
+    if (!showError(!("FormData" in window), "FormData is not supported in you browser")) {
 
         $.fn.extend({
             quickMultiFileUploader: function (options) {
-                this.each(function () {
-                    if (options === undefined) {
-                        options = {};
-                    }
 
-                    if (options && typeof (options) == 'object') {
-                        options = $.extend({}, options);
-                    }
-
-                    new $.quickMultiFileUploader(this, options);
+                var result = this.map(function () {
+                    return $.quickMultiFileUploader(this, options);
                 });
+
+                return result.length > 1 ? result : result[0];
             }
         });
 
         $.quickMultiFileUploader = function (elem, options) {
-            if (options && typeof (options) == 'string') {
-                if (options == 'upload') {
-                    startUpload();
-                } else {
-                    console.error("quickMultiFileUploader: " + options + " method not recognized");
-                }
-            }
-            else if (!$(elem).is('input') || $(elem).attr('type') != 'file') {
-                console.error("quickMultiFileUploader: this plugin is only valid for input file elements");
-            }
-            else if (!elem.hasAttribute("name")) {
-                console.error("quickMultiFileUploader: element must have name attribute");
-            }
-            else {
-                //setup
-                options = $.extend({}, $.quickMultiFileUploader.defaults, elem.dataset, options);
-                parseDataAttributes(options);
-                $(elem).data(internalPrefix, options);
+            var previousInstance = $(elem).data(internalPrefix);
 
-                if (options.uploadOnChange) {
-                    $(elem).on('change', function () {
-                        startUpload();
-                    });
-                }
-            }
-
-            function startUpload() {
-                if (elem.files.length > 0) {
-                    var options = $(elem).data(internalPrefix);
-
-                    if (!$.inArray("url", options)) {
-                        console.error("quickMultiFileUploader: the url must be set");
-                    }
-                    else {
-
-                        var elemName = $(elem).attr('name');
-                        var data = new FormData();
-
-                        for (var i = 0; i < elem.files.length; i++) {
-                            data.append(elemName, elem.files[i], elem.files[i].name);
-                        }
-
-                        var customData = $.extend({}, options.data);
-
-                        for (var dataKey in customData) {
-                            data.append(dataKey, customData[dataKey]);
-                        }
-
-                        $.ajax($.extend({}, options, { data: data }))
-                            .always(function () {
-                                afterUpload();
-                            }).fail(function () { console.log(4);});
-                    }
-                }
-            }
-
-            function afterUpload() {
-                elem.value = "";
-            }
-
-            function parseDataAttributes(dataAttributes) {
-                for (var key in dataAttributes) {
-                    if (dataAttributes[key] === "") {
-                        dataAttributes[key] = true;
-                    } else if (dataAttributes[key] == "false") {
-                        dataAttributes[key] = false;
-                    } else if (dataAttributes[key] == "true") {
-                        dataAttributes[key] = true;
-                    }
-                }
+            if (previousInstance !== undefined) {
+                previousInstance.setOptions(options);
+                return previousInstance;
+            } else {
+                return new quickMultiFileUploader(elem, options);
             }
         };
-        
+
         $.quickMultiFileUploader.defaults = {
-            uploadOnChange: false,
-            method: "post",
-            cache: false,
-            processData: false,
-            contentType: false
+            uploadOnChange: true,
+            resetAfterUpload: true,
+            useInputName: false,
+
+            //callbacks
+            beforeSend: function (elem, filenames, files) { return true; }, //if return false, ajax call doesn't start
+
+            //Ajax callbacks
+            onError: function (elem, jqXHR, textStatus, errorThrown) { },
+            onSuccess: function (elem, data, textStatus, jqXHR) { },
+            onComplete: function (elem, data, textStatus, jqXHR) { }
         };
 
         $(document).ready(function () {
